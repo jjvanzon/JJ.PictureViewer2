3fd74baf8af7ecab96d7792e6948cb29d53725fb
jjvanzon <janjoostvanzon@hotmail.com>
2020-12-05 14:05:03 +0100

    PictureViewer 2: Try fixing that PictureBox seemed to keep file in use, making moving file to category folder fail.

--- MainWindow.cs
+++ MainWindow.cs
@@ -21,293 +21,298 @@ using PictureViewer2.Properties;
 namespace PictureViewer2
 {
 
-    public partial class MainWindow : Form
-    {
+	public partial class MainWindow : Form
+	{
 
-        // Initialization & Finalization
+		// Initialization & Finalization
 
-        public MainWindow()
-        {
-            InitializeComponent();
-            CategoryList = CategoryListRepository.GetCurrentOrDefaultCategoryList();
-            BindKeyPress();
-            RequestDraw();
-        }
+		public MainWindow()
+		{
+			InitializeComponent();
+			CategoryList = CategoryListRepository.GetCurrentOrDefaultCategoryList();
+			BindKeyPress();
+			RequestDraw();
+		}
 
-        private void MainWindow_Load(object sender, EventArgs e)
-        {
-            ApplySettings();
-        }
+		private void MainWindow_Load(object sender, EventArgs e)
+		{
+			ApplySettings();
+		}
 
-        private void MainWindow_FormClosed(object sender, FormClosedEventArgs e)
-        {
-            SaveSettings();
-        }
+		private void MainWindow_FormClosed(object sender, FormClosedEventArgs e)
+		{
+			SaveSettings();
+		}
 
-        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            Close();
-        }
+		private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			Close();
+		}
 
-        // Data
+		// Data
 
-        private CategoryListRepository CategoryListRepository = new CategoryListRepository();
-        private CategoryList CategoryList;
+		private CategoryListRepository CategoryListRepository = new CategoryListRepository();
+		private CategoryList CategoryList;
 
-        // Selecting Items
+		// Selecting Items
 
-        private void folderPathTextBox_KeyPress(object sender, KeyPressEventArgs e)
-        {
-            if (e.KeyChar == 13)
-            {
-                SelectFolder(folderPathTextBox.Text);
-                fileListBox.Focus();
-            }
-        }
+		private void folderPathTextBox_KeyPress(object sender, KeyPressEventArgs e)
+		{
+			if (e.KeyChar == 13)
+			{
+				SelectFolder(folderPathTextBox.Text);
+				fileListBox.Focus();
+			}
+		}
 
-        private void folderTreeView_AfterSelect(object sender, TreeViewEventArgs e)
-        {
-            SelectFolder(folderTreeView.GetPath());
-        }
+		private void folderTreeView_AfterSelect(object sender, TreeViewEventArgs e)
+		{
+			SelectFolder(folderTreeView.GetPath());
+		}
 
-        private void SelectFolder(string path)
-        {
-            try
-            {
-                if (folderPathTextBox.Text != path) folderPathTextBox.Text = path;
-                if (folderTreeView.GetPath() != path) folderTreeView.SetPath(folderPathTextBox.Text);
-                fileListBox.Fill(folderPathTextBox.Text, resetSelection: true);
-            }
-            catch (DirectoryNotFoundException) { MessageBox.Show("The specified path does not exist", Program.ApplicationName); }
-            catch (NodeNotFoundApplicationException) { MessageBox.Show("Application error: an element of the path could not be opened in the folder explorer.", Program.ApplicationName); }
-            catch (IOException ex) { MessageBox.Show("Error: " + ex.Message, Program.ApplicationName); }       
-        }
+		private void SelectFolder(string path)
+		{
+			try
+			{
+				if (folderPathTextBox.Text != path) folderPathTextBox.Text = path;
+				if (folderTreeView.GetPath() != path) folderTreeView.SetPath(folderPathTextBox.Text);
+				fileListBox.Fill(folderPathTextBox.Text, resetSelection: true);
+			}
+			catch (DirectoryNotFoundException) { MessageBox.Show("The specified path does not exist", Program.ApplicationName); }
+			catch (NodeNotFoundApplicationException) { MessageBox.Show("Application error: an element of the path could not be opened in the folder explorer.", Program.ApplicationName); }
+			catch (IOException ex) { MessageBox.Show("Error: " + ex.Message, Program.ApplicationName); }       
+		}
 
-        private void fileListBox_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            ShowPicture(Path.Combine(folderPathTextBox.Text, (string)fileListBox.SelectedItem));
-        }
+		private void fileListBox_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			ShowPicture(Path.Combine(folderPathTextBox.Text, (string)fileListBox.SelectedItem));
+		}
 
-        private void SelectFile(int index)
-        {
-            if (fileListBox.SelectedIndex != index)
-            {
-                if (
-                    index >= 0 &&
-                    index < fileListBox.Items.Count
-                )
-                {
-                    fileListBox.SelectedIndex = index;
-                }
-            }
-        }
+		private void SelectFile(int index)
+		{
+			if (fileListBox.SelectedIndex != index)
+			{
+				if (
+					index >= 0 &&
+					index < fileListBox.Items.Count
+				)
+				{
+					fileListBox.SelectedIndex = index;
+				}
+			}
+		}
 
-        private void ShowPicture(string path)
-        {
-            try
-            {
-                pictureBox.Load(path);
-            }
-            catch
-            {
-                // Do not make error handling more specific than this. 
-                // Picture simply would not load. Wrong file type, wrong format, corrupt file, whatever.
-                // Do clear the previous image, though.
-                pictureBox.Image = null;
-            }
-        }
+		private void ShowPicture(string path)
+		{
+			try
+			{
+				// Try prevent keeping image file in use, by copyijng it.
+				using (Image imageFromFile = new Bitmap(path))
+				{
+					Image copiedImage = new Bitmap(imageFromFile);
+					pictureBox.Image = copiedImage;
+				}
+			}
+			catch (Exception ex)
+			{
+				// Do not make error handling more specific than this. 
+				// Picture simply would not load. Wrong file type, wrong format, corrupt file, whatever.
+				// Do clear the previous image, though.
+				pictureBox.Image = null;
+			}
+		}
 
-        // Keyboard Keys
+		// Keyboard Keys
 
-        private void BindKeyPress()
-        {
-            this.KeyPress += KeyPressHandler;
-            fileListBox.KeyPress += KeyPressHandler;
-        }
+		private void BindKeyPress()
+		{
+			this.KeyPress += KeyPressHandler;
+			fileListBox.KeyPress += KeyPressHandler;
+		}
 
-        private void KeyPressHandler(object sender, KeyPressEventArgs e)
-        {
-            ProcessKey(e.KeyChar);
-            e.Handled = true;
-        }
+		private void KeyPressHandler(object sender, KeyPressEventArgs e)
+		{
+			ProcessKey(e.KeyChar);
+			e.Handled = true;
+		}
 
-        private void ProcessKey(char KeyChar)
-        {
-            // Process Full Screen Keys
-            switch (KeyChar)
-            {
-                case (char)27: // Escape
-                    FullScreen = false;
-                    return;
-            }
-            Category category = CategoryList.FindCategoryByKey(KeyChar);
-            if (category != null)
-            {
-                try
-                {
-                    CategoryList.MoveToCategoryFolder(
-                        folderPathTextBox.Text,
-                        (string)fileListBox.SelectedItem,
-                        category);
-                }
-                catch (IOException) { MessageBox.Show("File access error. File may already exist.", Program.ApplicationName); }
-                catch (BadPathFormatApplicationException) { MessageBox.Show("Bad folder format. Check the category configuration.", Program.ApplicationName); }
-                fileListBox.Fill(folderPathTextBox.Text);
-                // Trick for getting the list box to focus again, so that the arrow keys will work.
-                fileListBox.Focus();
-                if (!fileListBox.Focused) { fileListBox.Visible = true; fileListBox.Focus(); fileListBox.Visible = false; }
-            }
-        }
+		private void ProcessKey(char KeyChar)
+		{
+			// Process Full Screen Keys
+			switch (KeyChar)
+			{
+				case (char)27: // Escape
+					FullScreen = false;
+					return;
+			}
+			Category category = CategoryList.FindCategoryByKey(KeyChar);
+			if (category != null)
+			{
+				try
+				{
+					CategoryList.MoveToCategoryFolder(
+						folderPathTextBox.Text,
+						(string)fileListBox.SelectedItem,
+						category);
+				}
+				catch (IOException ex ) { MessageBox.Show("File access error. File may already exist.", Program.ApplicationName); }
+				catch (BadPathFormatApplicationException ex) { MessageBox.Show("Bad folder format. Check the category configuration.", Program.ApplicationName); }
+				fileListBox.Fill(folderPathTextBox.Text);
+				// Trick for getting the list box to focus again, so that the arrow keys will work.
+				fileListBox.Focus();
+				if (!fileListBox.Focused) { fileListBox.Visible = true; fileListBox.Focus(); fileListBox.Visible = false; }
+			}
+		}
 
-        // Get arrow-key functionality when clicking picture
+		// Get arrow-key functionality when clicking picture
 
-        private void pictureBox_Click(object sender, EventArgs e)
-        {
-            fileListBox.Focus();
-        }
+		private void pictureBox_Click(object sender, EventArgs e)
+		{
+			fileListBox.Focus();
+		}
 
-        // Full Screen
+		// Full Screen
 
-        private void pictureBox_DoubleClick(object sender, EventArgs e)
-        {
-            FullScreen = !FullScreen;
-        }
+		private void pictureBox_DoubleClick(object sender, EventArgs e)
+		{
+			FullScreen = !FullScreen;
+		}
 
-        private void fullScreenToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            FullScreen = !FullScreen;
-        }
+		private void fullScreenToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			FullScreen = !FullScreen;
+		}
 
-        private bool _fullscreen;
-        private bool FullScreen
-        {
-            get
-            {
-                return _fullscreen;
-            }
-            set
-            {
-                if (_fullscreen != value)
-                {
-                    // Store
-                    _fullscreen = value;
-                    // Apply
-                    FormBorderStyle = _fullscreen ? FormBorderStyle.None : FormBorderStyle.Sizable;
-                    WindowState = _fullscreen ? FormWindowState.Maximized : FormWindowState.Normal;
-                    // Draw
-                    RequestDraw();
-                }
-            }
-        }
+		private bool _fullscreen;
+		private bool FullScreen
+		{
+			get
+			{
+				return _fullscreen;
+			}
+			set
+			{
+				if (_fullscreen != value)
+				{
+					// Store
+					_fullscreen = value;
+					// Apply
+					FormBorderStyle = _fullscreen ? FormBorderStyle.None : FormBorderStyle.Sizable;
+					WindowState = _fullscreen ? FormWindowState.Maximized : FormWindowState.Normal;
+					// Draw
+					RequestDraw();
+				}
+			}
+		}
 
-        // Resize
+		// Resize
 
-        private void MainForm_SizeChanged(object sender, EventArgs e)
-        {
-            RequestDraw();
-        }
+		private void MainForm_SizeChanged(object sender, EventArgs e)
+		{
+			RequestDraw();
+		}
 
-        private void RequestDraw()
-        {
-            drawTimer.Enabled = true;
-        }
+		private void RequestDraw()
+		{
+			drawTimer.Enabled = true;
+		}
 
-        private void drawTimer_Tick(object sender, EventArgs e)
-        {
-            Draw();
-        }
+		private void drawTimer_Tick(object sender, EventArgs e)
+		{
+			Draw();
+		}
 
-        private const int LeftPanelWidth = 300;
+		private const int LeftPanelWidth = 300;
 
-        private void Draw()
-        {
-            drawTimer.Enabled = false;
-            SuspendLayout();
-            // Visibility
-            folderPathTextBox.Visible = !FullScreen;
-            folderTreeView.Visible = !FullScreen;
-            // Do this last, so that fileListBox is the last control that was visible,
-            // so that it gets focus, even though it is invisible, so that the arrow keys
-            // will result in next-previous behavior.
-            fileListBox.Visible = !FullScreen; 
-            menuStrip.Visible = !FullScreen;
-            // Positioning
-            if (FullScreen)
-            {
-                pictureBox.Left = 0;
-                pictureBox.Top = 0;
-                pictureBox.Width = this.ClientSize.Width;
-                pictureBox.Height = this.ClientSize.Height;
-            }
-            else
-            {
-                folderPathTextBox.Left = 0;
-                folderPathTextBox.Top = menuStrip.Bottom;
-                folderPathTextBox.Width = LeftPanelWidth;
-                folderTreeView.Left = 0;
-                folderTreeView.Top = folderPathTextBox.Bottom;
-                folderTreeView.Width = LeftPanelWidth / 2;
-                folderTreeView.Height = this.ClientSize.Height - folderPathTextBox.Bottom;
-                fileListBox.Left = LeftPanelWidth / 2;
-                fileListBox.Top = folderPathTextBox.Bottom;
-                fileListBox.Width = LeftPanelWidth / 2;
-                fileListBox.Height = this.ClientSize.Height - fileListBox.Top;
-                pictureBox.Left = LeftPanelWidth;
-                pictureBox.Top = menuStrip.Bottom;
-                pictureBox.Width = this.ClientSize.Width - LeftPanelWidth;
-                pictureBox.Height = this.ClientSize.Height - menuStrip.Bottom;
-            }
-            ResumeLayout();
-        }
+		private void Draw()
+		{
+			drawTimer.Enabled = false;
+			SuspendLayout();
+			// Visibility
+			folderPathTextBox.Visible = !FullScreen;
+			folderTreeView.Visible = !FullScreen;
+			// Do this last, so that fileListBox is the last control that was visible,
+			// so that it gets focus, even though it is invisible, so that the arrow keys
+			// will result in next-previous behavior.
+			fileListBox.Visible = !FullScreen; 
+			menuStrip.Visible = !FullScreen;
+			// Positioning
+			if (FullScreen)
+			{
+				pictureBox.Left = 0;
+				pictureBox.Top = 0;
+				pictureBox.Width = this.ClientSize.Width;
+				pictureBox.Height = this.ClientSize.Height;
+			}
+			else
+			{
+				folderPathTextBox.Left = 0;
+				folderPathTextBox.Top = menuStrip.Bottom;
+				folderPathTextBox.Width = LeftPanelWidth;
+				folderTreeView.Left = 0;
+				folderTreeView.Top = folderPathTextBox.Bottom;
+				folderTreeView.Width = LeftPanelWidth / 2;
+				folderTreeView.Height = this.ClientSize.Height - folderPathTextBox.Bottom;
+				fileListBox.Left = LeftPanelWidth / 2;
+				fileListBox.Top = folderPathTextBox.Bottom;
+				fileListBox.Width = LeftPanelWidth / 2;
+				fileListBox.Height = this.ClientSize.Height - fileListBox.Top;
+				pictureBox.Left = LeftPanelWidth;
+				pictureBox.Top = menuStrip.Bottom;
+				pictureBox.Width = this.ClientSize.Width - LeftPanelWidth;
+				pictureBox.Height = this.ClientSize.Height - menuStrip.Bottom;
+			}
+			ResumeLayout();
+		}
 
-        // Settings
+		// Settings
 
-        private void ApplySettings()
-        {
-            // Folder
-            string folderPath = Settings.Default.FolderPath;
-            if (!String.IsNullOrWhiteSpace(folderPath))
-            {
-                SelectFolder(folderPath);
-            }
-            // File
-            SelectFile(Settings.Default.SelectedFileIndex);
-            // Window Positions
-            int width = Settings.Default.WindowWidth;
-            int height = Settings.Default.WindowHeight;
-            if (width > 0 && height > 0)
-            {
-                Left = Settings.Default.WindowLeft;
-                Top = Settings.Default.WindowTop;
-                Width = width;
-                Height = height;
-            }
-        }
+		private void ApplySettings()
+		{
+			// Folder
+			string folderPath = Settings.Default.FolderPath;
+			if (!String.IsNullOrWhiteSpace(folderPath))
+			{
+				SelectFolder(folderPath);
+			}
+			// File
+			SelectFile(Settings.Default.SelectedFileIndex);
+			// Window Positions
+			int width = Settings.Default.WindowWidth;
+			int height = Settings.Default.WindowHeight;
+			if (width > 0 && height > 0)
+			{
+				Left = Settings.Default.WindowLeft;
+				Top = Settings.Default.WindowTop;
+				Width = width;
+				Height = height;
+			}
+		}
 
-        private void SaveSettings()
-        {
-            Settings.Default.FolderPath = folderPathTextBox.Text;
-            Settings.Default.SelectedFileIndex = fileListBox.SelectedIndex;
-            if (this.WindowState == FormWindowState.Normal)
-            {
-                Settings.Default.WindowLeft = Left;
-                Settings.Default.WindowTop = Top;
-                Settings.Default.WindowWidth = Width;
-                Settings.Default.WindowHeight = Height;
-            }
-            Settings.Default.Save();
-        }
+		private void SaveSettings()
+		{
+			Settings.Default.FolderPath = folderPathTextBox.Text;
+			Settings.Default.SelectedFileIndex = fileListBox.SelectedIndex;
+			if (this.WindowState == FormWindowState.Normal)
+			{
+				Settings.Default.WindowLeft = Left;
+				Settings.Default.WindowTop = Top;
+				Settings.Default.WindowWidth = Width;
+				Settings.Default.WindowHeight = Height;
+			}
+			Settings.Default.Save();
+		}
 
-        // Categories
+		// Categories
 
-        private void categoriesToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            // Show 
-            var categoriesWindow = new CategoriesWindow();
-            categoriesWindow.ShowDialog();
-            // Reload category list
-            CategoryList = CategoryListRepository.GetCurrentOrDefaultCategoryList();
-        }
+		private void categoriesToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			// Show 
+			var categoriesWindow = new CategoriesWindow();
+			categoriesWindow.ShowDialog();
+			// Reload category list
+			CategoryList = CategoryListRepository.GetCurrentOrDefaultCategoryList();
+		}
 
-    }
+	}
 }
